8edb18cdd5ed38abf15db8dfe551ce8974aa1c8d
netbe <francois.benaiteau@gmail.com>
2014-04-11 20:17:37 +0200

    Initial Commit

---
+++ bam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bam
+//
+//  Created by François Benaiteau on 11/04/14.
+//  Copyright (c) 2014 François Benaiteau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
